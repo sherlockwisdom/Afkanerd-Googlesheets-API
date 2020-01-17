dc262c757fa49916b406855d8b8e133400741bf0
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-17 10:38:06 +0100

    added file changes

--- read_sheets.js
+++ read_sheets.js
@@ -13,10 +13,12 @@ exports.readSheet = ( spreadsheetId, range, auth ) => {
 		//TODO: Transform this information into a map, which allows searching
 		let sheetMap = new Map();
 		//console.log( result.values.data );
-		for(let i =1; i< result.data.length ; ++i) {
+		for( let i=0; i< result.data.values.length; ++i) {
+			let array = []
 			for( let j=0; j< result.data.values[i].length; ++j ) {
-				sheetMap.set( result.data.values[0][j], result.data.values[i][j] );	
+				array.push( result.data.values[j][i] );
 			}
+			sheetMap.set( result.data.values[0][i], array) ;
 		}
 		console.log( sheetMap );
 				
