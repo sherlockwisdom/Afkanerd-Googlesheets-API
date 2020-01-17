ad2c6f2684cb28be90a75a23b86ce92058f94c4e
Sherlock Wisdom <wisdomnji@gmail.com>
2020-01-17 08:43:21 +0100

    added some changes

--- read_sheets.js
+++ read_sheets.js
@@ -3,16 +3,21 @@ exports.readSheet = ( spreadsheetId, range, auth ) => {
 	const sheets = google.sheets({version: 'v4', auth});
 
 	sheets.spreadsheets.values.get({
-	  spreadsheetId,
-	  range,
+		spreadsheetId,
+		range,
 	}, (err, result) => {
-	  if (err) {
-	    // Handle error
-	    console.log(err);
-	  } else {
-		console.log( result.data );
-	    const numRows = result.values ? result.values.length : 0;
-	    console.log(`${numRows} rows retrieved.`);
-	  }
+	if (err) {
+		// Handle error
+		console.log(err);
+	} else {
+		//TODO: Transform this information into a map, which allows searching
+		let sheetMap = new Map();
+		for(let i =0; i< results.values[0].length ; ++i) {
+			for( let j=1; i< results.values.data.length; ++i ) {}
+		}
+				
+		const numRows = result.values ? result.values.length : 0;
+		console.log(`${numRows} rows retrieved.`);
+	}
 	});	
 }
